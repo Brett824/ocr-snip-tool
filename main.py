132e52ec794449a36244c76297f508ff81d58299
Brett824 <bsheingo@u.rochester.edu>
2022-04-18 23:43:28 -0400

    cleanup

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-from pynput.keyboard import GlobalHotKeys, Controller, Key, HotKey
+from pynput.keyboard import GlobalHotKeys
 from PyQt5 import QtWidgets
 from ocr_snip import OcrSnip
 import sys
 
 
 hotkey = ["<cmd_l>", "<shift>", "w"]
-app = None
 h = None
+
 
 # The function called when a hotkey is pressed
 def snip():
     global app
     print("Snip hotkey pressed")
-    keyboard = Controller()
     app = QtWidgets.QApplication(sys.argv)
     window = OcrSnip()
     window.show()
     app.aboutToQuit.connect(app.deleteLater)
     app.exit(app.exec_())
+    # state gets frozen during callback function so manually clear to avoid weird inputs
     if h:
         for hk in h._hotkeys:
             hk._state.clear()
